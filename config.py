15f45ef8089ea9894382081fa6b848bdb46df3ca
Mayank <mayankkanti2325@gmail.com>
2025-07-02 10:06:06 +0530

    fix: no more loading dotenv on Railway

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
 from os import environ, getenv
-
-from dotenv import load_dotenv
-
-load_dotenv()
+if getenv("RAILWAY_ENVIRONMENT"):
+    print("Running on Railway, not loading .env")
+else:
+    from dotenv import load_dotenv
+    load_dotenv()
 TOKEN = environ.get('TOKEN') # Should raise if missing
 DEV_USER_ID = int(getenv('DEV'))
 NCHANNEL = int(getenv('NOTICE_CHANNEL'))
