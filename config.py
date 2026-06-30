273a704e7d4ba851e28c9bb96f755d2046a32804
Mayank <mayankkanti2325@gmail.com>
2026-06-30 23:10:09 +0530

    feat: add Docker support

--- config.py
+++ config.py
@@ -1,11 +1,13 @@
 from os import getenv
+from dotenv import load_dotenv
 
-TOKEN = getenv('TOKEN')
-DEV = getenv('DEV')
-NCHANNEL = getenv('NOTICE_CHANNEL')
-LCHANNEL = getenv('LOG_CHANNEL')
-TASK_FREQUENCY = getenv('TASK_FREQUENCY')
+load_dotenv()
 
+TOKEN = getenv("TOKEN")
+DEV = getenv("DEV")
+NCHANNEL = getenv("NOTICE_CHANNEL")
+LCHANNEL = getenv("LOG_CHANNEL")
+TASK_FREQUENCY = getenv("TASK_FREQUENCY")
 
 DEV_USER_ID = int(DEV)
 NCHANNEL = int(NCHANNEL)
